83fbe0fe504a666b7e95a8a2da59edbfcedfc497
jghibiki <jghibiki.games@gmail.com>
2016-09-15 23:43:39 -0500

    adds new file popul menu

--- app/navigation/navigation.controller.js
+++ app/navigation/navigation.controller.js
@@ -5,9 +5,39 @@ angular.module('rpgBuddy.navigation')
 
 .controller("NavigationCtrl", NavigationCtrl)
 
+NavigationCtrl.$inject = [
+    "$scope",
+    "$mdDialog"
+]
+function NavigationCtrl($scope, $mdDialog){
 
-function NavigationCtrl(){
-    
+    $scope.showNewFileDialog = function(ev){
+        $mdDialog.show({
+            controller: DialogController,
+			templateUrl: 'navigation/new-file.navigation.controller.html',
+            targetEvent: ev,
+            clickOutsidetoClose: true
+        })
+    }
+
+	DialogController.$inject = [
+		"$scope",
+		"$mdDialog",
+	]
+	function DialogController($scope, $mdDialog) {
+
+		$scope.hide = function() {
+		  $mdDialog.hide();
+		};
+
+		$scope.cancel = function() {
+		  $mdDialog.cancel();
+		};
+
+		$scope.answer = function(answer) {
+		  $mdDialog.hide(answer);
+		};
+	}
 }
 
 
